e0b17539ae5c1f9cbded1bfa9a64af736f709ddc
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-10-06 15:02:58 +0500

    Added Named_Export.js

---
+++ Module 1 - React Components/1. Introduction/Named_Export.js
@@ -0,0 +1,15 @@
+export function addTwo (a, b) {
+    console.log(a + b);
+}
+
+export function addThree (a, b, c) {
+    console.log(a + b + c);
+}
+
+// OR 
+
+/*
+function addTwo (a, b) {...}
+
+export default addTwo;
+*/
