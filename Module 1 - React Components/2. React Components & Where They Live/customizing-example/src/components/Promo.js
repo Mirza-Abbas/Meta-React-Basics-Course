1d738d15741eeb8db2fe9b0c6af6fa77247ad0e1
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-10-08 16:54:14 +0500

    Added customizing-example

---
+++ Module 1 - React Components/2. React Components & Where They Live/customizing-example/src/components/Promo.js
@@ -0,0 +1,12 @@
+function Promo() {
+    return (
+        <div className="promo-section">
+            <div>
+                <h1>Don't Miss this Deal</h1>
+            </div>
+            <h2>Subscribe to Newsletter</h2>
+        </div>
+    );
+}
+
+export default Promo;
