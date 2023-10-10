9ac39c1cc45714406ed2d829188c37191f1e30d0
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-10-10 21:23:51 +0500

    Added Project Files

---
+++ Module 1 - React Components/3. Component Use & Styling/examples/src/App.js
@@ -0,0 +1,36 @@
+//Uncomment the imported component before running
+
+// import Bag from "./Bag";
+// import InlineStyle from "./Components/InlineStyle";
+// import FunctionExpression from "./Components/FunctionExpression";
+// import ArrowFunction1 from "./Components/ArrowFunction1";
+// import ArrowFunction2 from "./Components/ArrowFunction2";
+// import EmbeddedExpressions from "./Components/EmbeddedExpressions";
+// import EmbeddedFunctions from "./Components/EmbeddedFunctions";
+// import EmbeddedAttributes from "./Components/EmbeddedAttributes";
+// import TernaryExpressions from "./Components/TernaryExpressions";
+// import FunctionInvocation1 from "./Components/FunctionInvocation1";
+// import FunctionInvocation2 from "./Components/FunctionInvocation2";
+// import PropExpressions1 from "./Components/PropExpressions1";
+import PropExpressions2 from "./Components/PropExpressions2";
+
+
+function App() {
+  return (
+    // <Bag children="This is a Props Example"/>
+    // <InlineStyle />
+    // <FunctionExpression first="Home"/>
+    // <ArrowFunction1 first="Home"/>
+    // <ArrowFunction2 />
+    // <EmbeddedExpressions />
+    // <EmbeddedFunctions />
+    // <EmbeddedAttributes />
+    // <TernaryExpressions />
+    // <FunctionInvocation1 />
+    // <FunctionInvocation2 />
+    // <PropExpressions1/>
+    <PropExpressions2 />
+  );
+}
+
+export default App;
