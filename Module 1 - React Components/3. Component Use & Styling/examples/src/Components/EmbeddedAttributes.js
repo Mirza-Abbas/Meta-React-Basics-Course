3827c16ce0ab9454062949f834cd7f15dc78c8f1
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-10-14 12:21:19 +0500

    Added EmbeddedAttributes.js

---
+++ Module 1 - React Components/3. Component Use & Styling/examples/src/Components/EmbeddedAttributes.js
@@ -0,0 +1,9 @@
+const url = "https://www.pngwing.com/en/search?q=link";
+
+const image = <img src = {url} alt="React logo"/>;
+
+function EmbeddedAttributes(){
+    return image;
+}
+
+export default EmbeddedAttributes;
