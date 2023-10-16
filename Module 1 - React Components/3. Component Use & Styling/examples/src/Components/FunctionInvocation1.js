4944273e3c3c94902ddcbf6b629590debcf34ec8
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-10-16 20:34:16 +0500

    Added FunctionInvocation1.js

---
+++ Module 1 - React Components/3. Component Use & Styling/examples/src/Components/FunctionInvocation1.js
@@ -0,0 +1,10 @@
+function FunctionInvocation1 (){
+    return (
+        <div>
+            <h1>Here's a random number from 0 to 10: {Math.floor(Math.random() * 10) + 1}
+            </h1>
+        </div>
+    )
+}
+
+export default FunctionInvocation1;
