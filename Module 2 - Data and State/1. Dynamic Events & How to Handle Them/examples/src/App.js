c6b6fd48fbb2f5575b98ab7e14a830072736912f
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-10-19 20:05:22 +0500

    Added Project Files

---
+++ Module 2 - Data and State/1. Dynamic Events & How to Handle Them/examples/src/App.js
@@ -0,0 +1,32 @@
+//Uncomment the imported component before running
+
+// import OnClick from './Components/OnClick';
+// import OnMouseOver from './Components/OnMouseOver';
+// import ES5_Function from './Components/ES5_Function';
+// import ES6_Function from './Components/ES6_Function';
+// import Function_Declaration from './Components/Function_Declaration';
+// import Function_Expression from './Components/Function_Expression';
+import UserEvents from './UserEvents';
+import './App.css';
+
+function App() {
+  return (
+    // <OnClick />
+
+    // <OnMouseOver />
+    
+    // <ES5_Function />
+    
+    // <ES6_Function />
+
+    // <button onClick={Function_Declaration}>
+    //   Click Me!
+    // </button>
+
+    // <Function_Expression />
+
+    <UserEvents />
+  );
+}
+
+export default App;
