dde6fadb0a6603ac7d2df52ff550670eb373dbd8
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-10-21 11:45:43 +0500

    Added ES6_Function.js

---
+++ Module 2 - Data and State/1. Dynamic Events & How to Handle Them/examples/src/Components/ES6_Function.js
@@ -0,0 +1,11 @@
+// Handling Event using inline anonymous ES6 functions:
+
+function ES6_Function (){
+    return (
+        <button onClick={() => console.log("Clicked")}>
+            Click Me!
+        </button>
+    )
+}
+
+export default ES6_Function;
