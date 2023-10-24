010fdc358e66eceefedf4ab27742990905762585
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-10-24 10:49:01 +0500

    Added Project Files

---
+++ Module 2 - Data and State/2. Data and Events/examples/src/App.js
@@ -0,0 +1,36 @@
+//Uncomment the imported component before running
+
+// import Parent from './Components/Parent';
+// import State from './Components/State';
+// import UseState_Hook from "./Components/UseState_Hook";
+// import Stateless_Component from "./Components/Stateless_Component";
+// import Stateful_Component from "./Components/Stateful_Component";
+// import UpdateState from "./Components/UpdateState";
+// import PropDrilling from "./Components/PropDrilling";
+import UseState_Hook from "./Components/UseReducer_Hook";
+
+function App() {
+
+  // const date = new Date();
+
+  return (
+
+    // <Parent />
+
+    // <State date={date.toLocaleTimeString()} />
+    
+    // <UseState_Hook />
+    
+    // <Stateless_Component />
+    
+    // <Stateful_Component word="Hello" />
+
+    // <UpdateState />
+
+    // <PropDrilling msg={"I passed through the Header and the Wrapper and I reached the Button component"} />
+
+    <UseState_Hook />
+  );
+}
+
+export default App;
