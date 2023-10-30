be97bf0c23a23fbac471cad83e528176d05867bd
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-10-30 12:04:11 +0500

    Added Project Files

---
+++ Module 3 - Navigation, Updating & Assets in React.js/1. Linking & Routing/examples/src/App.js
@@ -0,0 +1,36 @@
+// import Homepage from "./Components/Homepage";
+// import AboutMe from "./Components/AboutMe";
+// import {Routes, Route, Link} from "react-router-dom";
+
+// import TernaryOperator from "./Components/TernaryOperator";
+// import ConditionalRendering from "./Components/ConditionalRendering";
+// import ElementVariables from "./Components/ElementVariables";
+// import LogicalOperator from "./Components/LogicalOperator";
+import ConditionalComponents from "./Components/ConditionalComponents";
+
+function App() {
+  return (
+    // <div>
+    //   <nav classname="nav">
+    //     <Link to="/" classname="nav-item">Homepage</Link>
+    //     <Link to="/about-me" classname="nav-item">About Me</Link>
+    //   </nav>
+    //   <Routes>
+    //     <Route path="/" element={<Homepage />} />
+    //     <Route path="/about-me" element={<AboutMe />} />
+    //   </Routes>
+    // </div>
+
+    // <TernaryOperator />
+    
+    // <ConditionalRendering />
+
+    // <ElementVariables  day={"6"}/>
+
+    // <LogicalOperator />
+
+    <ConditionalComponents day={"6"}/>
+  );
+}
+
+export default App;
