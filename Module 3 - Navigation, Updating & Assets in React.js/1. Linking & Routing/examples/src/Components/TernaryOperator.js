92083eaacc88f0ab50aac45a042bf6bed91613c8
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-11-01 22:24:00 +0500

    Added TernaryOperator.js

---
+++ Module 3 - Navigation, Updating & Assets in React.js/1. Linking & Routing/examples/src/Components/TernaryOperator.js
@@ -0,0 +1,13 @@
+function TernaryOperator () {
+    const summer=true;
+
+    return(
+    <div>
+        {summer ? 
+        <h1>Let's go to the beach</h1> :
+        <h1>Let's watch TV</h1>}
+    </div>
+    )
+}
+
+export default TernaryOperator; 
