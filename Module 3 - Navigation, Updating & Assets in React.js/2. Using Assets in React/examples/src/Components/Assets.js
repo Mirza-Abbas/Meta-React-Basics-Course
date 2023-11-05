152611400e9ce4470850613e5d361bca42a4179b
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-11-05 12:28:55 +0500

    Added Assets.js

---
+++ Module 3 - Navigation, Updating & Assets in React.js/2. Using Assets in React/examples/src/Components/Assets.js
@@ -0,0 +1,11 @@
+import Logo from '../Assets/logo512.png';
+
+function Assets(){
+    return(
+        <div>
+            <img src={Logo} alt="React"/>
+        </div>
+    )
+}
+
+export default Assets
