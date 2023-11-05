26597b0e7a72438b33b8240eb3cca56ef0d7ad18
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-11-05 12:27:51 +0500

    Added Project Files

---
+++ Module 3 - Navigation, Updating & Assets in React.js/2. Using Assets in React/examples/src/App.js
@@ -0,0 +1,20 @@
+// import Assets from "./Components/Assets";
+// import Require from "./Components/Require";
+// import ImageURL from "./Components/ImageURL";
+import VideoPlayer from "./Components/VideoPlayer";
+
+
+function App() {
+  return ( 
+
+    // <Assets />
+
+    // <Require />
+
+    // <ImageURL />
+
+    <VideoPlayer />
+  );
+}
+
+export default App;
