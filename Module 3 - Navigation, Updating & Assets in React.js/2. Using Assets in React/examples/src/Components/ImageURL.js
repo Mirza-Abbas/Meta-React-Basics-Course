5ecf3585cc24bd2031d7156c00606a57c0100690
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-11-06 18:56:29 +0500

    Added ImageURL.js

---
+++ Module 3 - Navigation, Updating & Assets in React.js/2. Using Assets in React/examples/src/Components/ImageURL.js
@@ -0,0 +1,11 @@
+function ImageURL () {
+    const URL="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg";
+
+    return(
+        <div>
+            <img src={URL} height={400} width={400} alt="React Framework"/>
+        </div>
+    )
+}
+
+export default ImageURL;
